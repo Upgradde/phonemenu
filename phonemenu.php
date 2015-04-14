7fa3ad1ccd6b4b1ec09f79365479255684ece221
Winfield Little <win@winfieldlittle.com>
2015-04-14 15:05:29 -0500

    Made update phonemenu.php

--- phonemenu.php
+++ phonemenu.php
@@ -24,32 +24,32 @@ header("content-type: text/xml");
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response>\n";
 switch($destination) {
 	case 'hours': ?>
-		<Say>Initech is open Monday through Friday, 9am to 5pm</Say>
+		<Say>Joey press is open Monday through Friday, 9am to 5pm</Say>
 		<Say>Saturday, 10am to 3pm and closed on Sundays</Say>
 		<?php break;
 	case 'location': ?>
-		<Say>Initech is located at 101 4th St in San Francisco California</Say>
+		<Say>Joey Press is located at 21650 Milsa Drive in San Antonio Texas</Say>
 		<Gather action="<?php echo 'http://' . dirname($_SERVER["SERVER_NAME"] .  $_SERVER['PHP_SELF']) . '/phonemenu.php?node=location'; ?>" numDigits="1">
-			<Say>For directions from the East Bay, press 1</Say>
-			<Say>For directions from San Jose, press 2</Say>
+			<Say>For directions from the Austin, press 1</Say>
+			<Say>For directions from San Bernny, press 2</Say>
 		</Gather>
 		<?php break;
 	case 'east-bay': ?>
-		<Say>Take BART towards San Francisco / Milbrae. Get off on Powell Street. Walk a block down 4th street</Say>
+		<Say>Take Interstate Highway 10 West towards San Antonio. Exit Dominion Drive and take the turnaround. Turn right on Milsa.  We are right behind the Lexus deakership</Say>
 		<?php break;
 	case 'san-jose': ?>
-		<Say>Take Cal Train to the Milbrae BART station. Take any Bart train to Powell Street</Say>
+		<Say>Take Interstate Highway 10 East</Say>
 		<?php break;
 	case 'duck'; ?>
-		<Play>hello.mp3</Play>
+		<Play>duck.mp3</Play>
 		<?php break;
 	case 'receptionist'; ?>
 		<Say>Please wait while we connect you</Say>
-		<Dial>NNNNNNNNNN</Dial>
+		<Dial>+12104522029</Dial>
 		<?php break;
 	default: ?>
 		<Gather action="<?php echo 'http://' . dirname($_SERVER["SERVER_NAME"] .  $_SERVER['PHP_SELF']) . '/phonemenu.php?node=default'; ?>" numDigits="1">
-			<Say>Hello and welcome to the Initech Phone Menu</Say>
+			<Say>Hello and welcome to the Joey Press Phone Menu</Say>
 			<Say>For business hours, press 1</Say>
 			<Say>For directions, press 2</Say>
 			<Say>To hear a duck quack, press 3</Say>
